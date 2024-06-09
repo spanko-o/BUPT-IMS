b6371051854bbafe21a2220c9e0377c3f0edd2c4
Tao <2692772023@qq.com>
2024-06-09 13:17:37 +0800

    add admin

--- urls.py
+++ urls.py
@@ -1,8 +1,13 @@
 from applications.User.views.user_login_view import UserLoginView
 from applications.User.views.user_signup_view import UserSignupView
 
+from applications.Admin.views.admin_signup_view import AdminSignupView
+
 url_patterns = {
+    # User
     '/user/signup': UserSignupView,
     '/user/login': UserLoginView,
 
+    # Admin
+    '/admin/signup': AdminSignupView,
 }
